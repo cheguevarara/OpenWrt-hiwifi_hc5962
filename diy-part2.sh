aeeef41fb424dc92a5417023190a6fc182a585dc
cheguevarara <124117697+cheguevarara@users.noreply.github.com>
2024-01-14 11:31:51 +0800

    Update diy-part2.sh
    used by B70

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改默认 IP
 sed -i 's/192.168.1.1/192.168.1.21/g' package/base-files/files/bin/config_generate
+
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Gee B70'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+sed -i "s/OpenWrt /Z@r0 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+ 
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
